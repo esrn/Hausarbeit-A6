673ad6baec3d23d707f21149d660c6f5c9a9e716
Emre <esrn@outlook.de>
2022-09-09 19:21:23 +0200

    formatiert

--- main_a6.c
+++ main_a6.c
@@ -4,39 +4,39 @@
 
 void help()
 {
-	printf("Dieses Programm gibt Ihnen die Zeilennummer und die Länge der längesten Reihe aufeinanderfolgender Ziffern an. Die Ziffern sollen hierzu in einem Textdokument (.txt-Datei) jeweils mit einem Zeilenumbruch getrennt übergeben werden.");
-	printf("Bitte nutzen Sie folgende Argumente: \n");
-	printf("-h: Hilfe \n");
-	printf("-p: Angabe des Dateipfads \n");
+    printf("Dieses Programm gibt Ihnen die Zeilennummer und die Länge der längesten Reihe aufeinanderfolgender Ziffern an. Die Ziffern sollen hierzu in einem Textdokument (.txt-Datei) jeweils mit einem Zeilenumbruch getrennt übergeben werden.");
+    printf("Bitte nutzen Sie folgende Argumente: \n");
+    printf("-h: Hilfe \n");
+    printf("-p: Angabe des Dateipfads \n");
 }
 
 int main(int argc, char* argv[])
 {
-	int opt;
-	char *file;
+    int opt;
+    char *file;
 
-	while((opt=getopt(argc,argv,"hp:"))!=-1)
-	{
-		switch (opt){
-			case 'h':
-				help();
-				return 0;
-				break;
-			case 'p':
-				file = optarg;
-				break;
-		}
-	}
+    while((opt=getopt(argc,argv,"hp:"))!=-1)
+    {
+        switch (opt) {
+        case 'h':
+            help();
+            return 0;
+            break;
+        case 'p':
+            file = optarg;
+            break;
+        }
+    }
 
-	FILE *input_stream;
-	input_stream=fopen(file,"r");
+    FILE *input_stream;
+    input_stream=fopen(file,"r");
 
-	if(input_stream==NULL)
-	{
-		printf("Datei nicht vorhanden. \n");
-		return 0;
-	}
-	longest_consecutive_sequence(input_stream);
-	fclose(input_stream);
-	return 0;
+    if(input_stream==NULL)
+    {
+        printf("Datei nicht vorhanden. \n");
+        return 0;
+    }
+    longest_consecutive_sequence(input_stream);
+    fclose(input_stream);
+    return 0;
 }
